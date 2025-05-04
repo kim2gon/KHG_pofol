bef10b5b574b7af70ff8fbf41165f2dcb0a3d788
seik - 집 <seik1224@naver.com>
2025-05-04 16:51:25 +0900

    .

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import { useRecoilValue } from 'recoil'
-import { headerColorState } from '../store';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { headerColorState } from "../store";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-const headerColor = useRecoilValue(headerColorState) 
+  const headerColor = useRecoilValue(headerColorState);
+  console.log(headerColor);
 
   return (
     // <nav className='flex items-center justify-between gap-9 top-0 w-[1300px] max-w-[100vw] max-h-[100px] py-[29px] px-2 fixed z-30'>
-    <nav className='fixed top-0 left-0 right-0 max-w-[1300px] mx-auto flex items-center justify-between p-[8px] h-[100px] z-20' style={{background:`${headerColor}`}}>
-      <Link href='/' className='text-[30px] text-black text-left'>HEADER</Link>
-      <div className='text-[30px] bg-yellow-300 text-black'>SNS IMG</div>
-      <Link href='/' className='pt-4 pb-[10px] px-7 text-white bg-black relative min-w-[16px] text-center text-sm leading-tight font-semibold'>KHG PortFolio</Link>
+    <nav
+      className="fixed top-0 left-0 right-0 max-w-[1300px] mx-auto flex items-center justify-between p-[8px] h-[100px] z-20"
+      style={{ background: headerColor }}
+    >
+      <Link href="/" className="text-[30px] text-black text-left">
+        HEADER
+      </Link>
+      <div className="text-[30px] bg-yellow-300 text-black">SNS IMG</div>
+      <Link
+        href="/"
+        className="pt-4 pb-[10px] px-7 text-white bg-black relative min-w-[16px] text-center text-sm leading-tight font-semibold"
+      >
+        KHG PortFolio
+      </Link>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/sections/section5.js
+++ src/sections/section5.js
@@ -1,67 +1,100 @@
-import React from 'react'
-import aniimg1 from '../assets/img/earth.png'
-import aniimg2 from '../assets/img/egg1.png'
-import aniimg3 from '../assets/img/egg2.png'
-import aniimg4 from '../assets/img/flower.png'
-import aniimg5 from '../assets/img/pokeballimg.png'
-import aniimg6 from '../assets/img/robot.png'
-import { Link } from 'react-router-dom'
-
+import React, { useEffect, useRef } from "react";
+import aniimg1 from "../assets/img/earth.png";
+import aniimg2 from "../assets/img/egg1.png";
+import aniimg3 from "../assets/img/egg2.png";
+import aniimg4 from "../assets/img/flower.png";
+import aniimg5 from "../assets/img/pokeballimg.png";
+import aniimg6 from "../assets/img/robot.png";
+import { Link } from "react-router-dom";
+import { useInView } from "framer-motion";
+import { useSetRecoilState } from "recoil";
+import { currentSectionsState, headerColorState } from "../store";
 
 const Section5 = () => {
   const cssanimation = [
-    aniimg1, aniimg2, aniimg3, aniimg4, aniimg5, aniimg6, aniimg1, aniimg2, aniimg3, aniimg4, aniimg5, aniimg6
+    aniimg1,
+    aniimg2,
+    aniimg3,
+    aniimg4,
+    aniimg5,
+    aniimg6,
+    aniimg1,
+    aniimg2,
+    aniimg3,
+    aniimg4,
+    aniimg5,
+    aniimg6,
   ];
   // 12*2 24개 애니메이션 반복
 
+  const ref = useRef(null);
+  const isInView = useInView(ref, {
+    amount: 0.5,
+    once: false,
+  });
+
+  const setheaderColor = useSetRecoilState(headerColorState);
+
+  useEffect(() => {
+    if (isInView) {
+      setheaderColor("white");
+    } else {
+      setheaderColor("");
+    }
+  }, [isInView, setheaderColor]);
+
   return (
-    <section className='w-full h-dvh flex pt-[100px] pl-10 pb-[75px] !left-0 top-0 bottom-0 start-[--h-padding] end-[--h-padding] relative'>
-      <p className='mt-[15vh] m-0 max-w-60 font-medium text-[11px] leading-[18px] tracking-wide text-[#111]'>
+    <section
+      ref={ref}
+      className="w-full h-dvh flex pt-[100px] pl-10 pb-[75px] !left-0 top-0 bottom-0 start-[--h-padding] end-[--h-padding] relative"
+    >
+      <p className="mt-[15vh] m-0 max-w-60 font-medium text-[11px] leading-[18px] tracking-wide text-[#111]">
         this is my portfolio thank you for visiting my site thank you.
-        <Link href='/' className='block max-w-max min-w-40 mt-8 cursor-pointer font-semibold text-xs leading-tight text-center realative text-white pt-4 px-[30px] pb-[11px] bg-black'>
+        <Link
+          href="/"
+          className="block max-w-max min-w-40 mt-8 cursor-pointer font-semibold text-xs leading-tight text-center realative text-white pt-4 px-[30px] pb-[11px] bg-black"
+        >
           click my profile
         </Link>
       </p>
 
-      <div className='absolute top-0 bottom-[-75px] left-[326px] w-[324px] overflow-hidden'>
-        <div className='w-full h-full'>
-          <div className='top-full flex-col absolute flex slide-up'>
-            <div className='w-full'>
+      <div className="absolute top-0 bottom-[-75px] left-[326px] w-[324px] overflow-hidden">
+        <div className="w-full h-full">
+          <div className="top-full flex-col absolute flex slide-up">
+            <div className="w-full">
               {cssanimation.map((v, i) => (
-                <img key={v} src={v} className='block mt-[91px] object-contain w-[324px] h-[404px]' />
+                <img key={v} src={v} className="block mt-[91px] object-contain w-[324px] h-[404px]" />
               ))}
             </div>
-            <div className='w-full'>
+            <div className="w-full">
               {cssanimation.map((v, i) => (
-                <img key={v} src={v} className='block mt-[91px] object-contain w-[324px] h-[404px]' />
+                <img key={v} src={v} className="block mt-[91px] object-contain w-[324px] h-[404px]" />
               ))}
-              <div className='flex flex-wrap'>
-              </div>
+              <div className="flex flex-wrap"></div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className='absolute top-0 bottom-[-300px] left-[739px] w-[324px] overflow-hidden'>
-        <div className='w-full h-full'>
-          <div className='bottom-full flex-col absolute flex slide-down'>
-            <div className='w-full'>
+      <div className="absolute top-0 bottom-[-300px] left-[739px] w-[324px] overflow-hidden">
+        <div className="w-full h-full">
+          <div className="bottom-full flex-col absolute flex slide-down">
+            <div className="w-full">
               {cssanimation.map((v, i) => (
-                <img key={v} src={v} className='block mt-[91px] object-contain w-[324px] h-[404px]' />
+                <img key={v} src={v} className="block mt-[91px] object-contain w-[324px] h-[404px]" />
               ))}
             </div>
-            <div className='w-full'>
+            <div className="w-full">
               {cssanimation.map((v, i) => (
-                <img key={v} src={v} className='block mt-[91px] object-contain w-[324px] h-[404px]' />
+                <img key={v} src={v} className="block mt-[91px] object-contain w-[324px] h-[404px]" />
               ))}
-              <div className='flex flex-wrap'>
-              </div>
+              <div className="flex flex-wrap"></div>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Section5;
