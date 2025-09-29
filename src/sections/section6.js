8c9492a7842608328f4a76c10f5af1a6bf463599
[kim2gon] <[heegonkim2001@gmail.com]>
2025-09-30 02:54:42 +0900

    수정

--- src/sections/section6.js
+++ src/sections/section6.js
@@ -1,51 +1,37 @@
 import { motion, useAnimation } from "framer-motion";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const designWidth = 1440;
-const designHeight = 1024;
-
-const Bubble = ({ children, xRatio = 0, yRatio = 0, containerRef }) => {
+const Bubble = ({ children, x = 0, y = 0 }) => {
   const controls = useAnimation();
   const [isHovered, setIsHovered] = useState(false);
-  const initialY = useRef(0);
-  const [pos, setPos] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const updatePos = () => {
-      if (!containerRef.current) return;
-
-      const { clientWidth, clientHeight } = containerRef.current;
-
-      const x = clientWidth * xRatio;
-      const y = clientHeight * yRatio;
-
-      setPos({ x, y });
-      initialY.current = y;
-    };
-
-    updatePos();
-    window.addEventListener("resize", updatePos);
-    return () => window.removeEventListener("resize", updatePos);
-  }, [xRatio, yRatio, containerRef]);
+  const initialY = useRef(y);
 
   useEffect(() => {
     if (isHovered) {
       controls.start({
         scale: 1.5,
         opacity: 0,
-        transition: { duration: 0.15, ease: "easeOut" },
+        transition: { duration: 0.15, ease: 'easeOut' }
       });
 
       const isDown = setTimeout(() => {
-        controls.start({ y: window.innerHeight * 2 });
+        controls.start({
+          y: window.innerHeight * 2
+        })
       }, 1000);
 
       const isUp = setTimeout(() => {
-        controls.start({ scale: 1, opacity: 1 });
-      }, 1100);
+        controls.start({
+          scale: 1,
+          opacity: 1
+        });
+      }, 1100)
 
       const timeout = setTimeout(() => {
-        controls.start({ y: initialY.current, transition: { duration: 5 } });
+        controls.start({
+          y: initialY.current,
+          transition: { duration: 5 }
+        });
         setIsHovered(false);
       }, 3000);
 
@@ -57,56 +43,50 @@ const Bubble = ({ children, xRatio = 0, yRatio = 0, containerRef }) => {
     <motion.div
       animate={controls}
       onMouseEnter={() => setIsHovered(true)}
-      style={{ x: pos.x, y: pos.y }}
+      style={{
+        x, y
+      }}
       className="circlecontainer z-40 opacity-100"
     >
-      <div className="circleinner">{children}</div>
+      <div className="circleinner">
+        {children}
+      </div>
     </motion.div>
   );
 };
 
-const Bubble2 = ({ children, xRatio = 0, yRatio = 0, containerRef }) => {
+
+const Bubble2 = ({ children, x = 0, y = 0 }) => {
   const controls = useAnimation();
   const [isHovered, setIsHovered] = useState(false);
-  const initialY = useRef(0);
-  const [pos, setPos] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const updatePos = () => {
-      if (!containerRef.current) return;
-
-      const { clientWidth, clientHeight } = containerRef.current;
-
-      const x = clientWidth * xRatio;
-      const y = clientHeight * yRatio;
-
-      setPos({ x, y });
-      initialY.current = y;
-    };
-
-    updatePos();
-    window.addEventListener("resize", updatePos);
-    return () => window.removeEventListener("resize", updatePos);
-  }, [xRatio, yRatio, containerRef]);
+  const initialY = useRef(y);
 
   useEffect(() => {
     if (isHovered) {
       controls.start({
         scale: 1.5,
         opacity: 0,
-        transition: { duration: 0.15, ease: "easeOut" },
+        transition: { duration: 0.15, ease: 'easeOut' }
       });
 
       const isDown = setTimeout(() => {
-        controls.start({ y: window.innerHeight * 2 });
+        controls.start({
+          y: window.innerHeight * 2
+        })
       }, 1000);
 
       const isUp = setTimeout(() => {
-        controls.start({ scale: 1, opacity: 1 });
-      }, 1100);
+        controls.start({
+          scale: 1,
+          opacity: 1
+        });
+      }, 1100)
 
       const timeout = setTimeout(() => {
-        controls.start({ y: initialY.current, transition: { duration: 5 } });
+        controls.start({
+          y: initialY.current,
+          transition: { duration: 5 }
+        });
         setIsHovered(false);
       }, 3000);
 
@@ -118,57 +98,51 @@ const Bubble2 = ({ children, xRatio = 0, yRatio = 0, containerRef }) => {
     <motion.div
       animate={controls}
       onMouseEnter={() => setIsHovered(true)}
-      style={{ x: pos.x, y: pos.y }}
+      style={{
+        x, y
+      }}
       className="circlecontainer z-40 opacity-100"
     >
-      <div className="circleinner">{children}</div>
+      <div className="circleinner">
+        {children}
+      </div>
       <div className="dotspin"></div>
     </motion.div>
   );
 };
 
-const Circle = ({ children, xRatio = 0, yRatio = 0, containerRef }) => {
+
+const Circle = ({ children, x = 0, y = 0 }) => {
   const controls = useAnimation();
   const [isClicked, setIsClicked] = useState(false);
-  const initialY = useRef(0);
-  const [pos, setPos] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const updatePos = () => {
-      if (!containerRef.current) return;
-
-      const { clientWidth, clientHeight } = containerRef.current;
-
-      const x = clientWidth * xRatio;
-      const y = clientHeight * yRatio;
-
-      setPos({ x, y });
-      initialY.current = y;
-    };
-
-    updatePos();
-    window.addEventListener("resize", updatePos);
-    return () => window.removeEventListener("resize", updatePos);
-  }, [xRatio, yRatio, containerRef]);
+  const initialY = useRef(y);
 
   useEffect(() => {
     if (isClicked) {
       controls.start({
         scale: 1.5,
         opacity: 0,
-        transition: { duration: 0.15, ease: "easeOut" },
+        transition: { duration: 0.15, ease: 'easeOut' }
       });
 
       const isDown = setTimeout(() => {
-        controls.start({ y: window.innerHeight * 2 });
+        controls.start({
+          y: window.innerHeight * 2
+        })
       }, 1000);
 
       const isUp = setTimeout(() => {
-        controls.start({ scale: 1, opacity: 1 });
-      }, 1100);
+        controls.start({
+          scale: 1,
+          opacity: 1
+        });
+      }, 1100)
 
       const timeout = setTimeout(() => {
-        controls.start({ y: initialY.current, transition: { duration: 5 } });
+        controls.start({
+          y: initialY.current,
+          transition: { duration: 5 }
+        });
         setIsClicked(false);
       }, 3000);
 
@@ -180,7 +154,9 @@ const Circle = ({ children, xRatio = 0, yRatio = 0, containerRef }) => {
     <motion.div
       animate={controls}
       onMouseDown={() => setIsClicked(true)}
-      style={{ x: pos.x, y: pos.y }}
+      style={{
+        x, y
+      }}
       className="circlecontainer z-30 opacity-100"
     >
       <div className="circlespin">{children}</div>
@@ -188,72 +164,66 @@ const Circle = ({ children, xRatio = 0, yRatio = 0, containerRef }) => {
   );
 };
 
-const Section6 = () => {
-  const containerRef = useRef(null);
 
+const Section6 = () => {
   return (
     <section className="w-full h-dvh flex flex-col pt-[100px] pl-10 pb-[75px] !left-0 top-0 bottom-0 start-[--h-padding] end-[--h-padding] relative overflow-hidden">
-      <h3 className="font-bold text-2xl leading-tight mt-[10vh] mx-0 mb-[21px] text-[#111]">
-        My skills
-      </h3>
-      <p className="m-0 max-w-60 font-medium text-sm leading-[18px] tracking-wide">
-        제가 가진 기술들을 애니메이션을 이용하여 재밌게 표현해보았습니다.
+      <h3 className="font-bold text-2xl leading-tight mt-[10vh] mx-0 mb-[21px] text-[#111]">My skills</h3>
+      <p className="m-0 max-w-60 font-medium text-xs leading-[18px] tracking-wide">
+        제가 가진 기술들을 애니메이션을 이용하여 재밌게 표현했습니다.
       </p>
-      <div
-        ref={containerRef}
-        className="absolute -left-[70px] bottom-0 right-0 h-[1000px] overflow-hidden"
-      >
-        <Bubble2 xRatio={668 / designWidth} yRatio={380 / designHeight} containerRef={containerRef}>
+      <div className="absolute left-0 bottom-0 right-0 h-[1000px] overflow-hidden">
+        <Bubble2 x={668} y={380}>
           HTML
         </Bubble2>
-        <Bubble xRatio={821 / designWidth} yRatio={512 / designHeight} containerRef={containerRef}>
+        <Bubble x={821} y={512}>
           CSS
         </Bubble>
-        <Bubble xRatio={552 / designWidth} yRatio={553 / designHeight} containerRef={containerRef}>
+        <Bubble x={552} y={553}>
           JavaScript
         </Bubble>
-        <Bubble xRatio={1031 / designWidth} yRatio={619 / designHeight} containerRef={containerRef}>
+        <Bubble x={1031} y={619}>
           JQuery
         </Bubble>
-        <Bubble2 xRatio={264 / designWidth} yRatio={656 / designHeight} containerRef={containerRef}>
+        <Bubble2 x={264} y={656}>
           PhotoShop
         </Bubble2>
-        <Bubble xRatio={871 / designWidth} yRatio={720 / designHeight} containerRef={containerRef}>
+        <Bubble x={871} y={720}>
           Illustrator
         </Bubble>
-        <Bubble xRatio={642 / designWidth} yRatio={732 / designHeight} containerRef={containerRef}>
+        <Bubble x={642} y={732}>
           Figma
         </Bubble>
-        <Bubble2 xRatio={1193 / designWidth} yRatio={732 / designHeight} containerRef={containerRef}>
+        <Bubble2 x={1193} y={732}>
           React
         </Bubble2>
-        <Circle xRatio={710 / designWidth} yRatio={803 / designHeight} containerRef={containerRef} />
-        <Bubble xRatio={1248 / designWidth} yRatio={824 / designHeight} containerRef={containerRef}>
+        <Circle x={710} y={803}></Circle>
+        <Bubble x={1248} y={824}>
           Gsap
         </Bubble>
-        <Bubble xRatio={502 / designWidth} yRatio={836 / designHeight} containerRef={containerRef}>
+        <Bubble x={502} y={836}>
           Fiber
         </Bubble>
-        <Bubble xRatio={1031 / designWidth} yRatio={881 / designHeight} containerRef={containerRef}>
+        <Bubble x={1031} y={881}>
           Drei
         </Bubble>
-        <Bubble2 xRatio={306 / designWidth} yRatio={941 / designHeight} containerRef={containerRef}>
+        <Bubble2 x={306} y={941}>
           FramerMotion
         </Bubble2>
-        <Bubble2 xRatio={648 / designWidth} yRatio={941 / designHeight} containerRef={containerRef}>
+        <Bubble2 x={648} y={941}>
           ThreeJS
         </Bubble2>
-        <Bubble xRatio={1171 / designWidth} yRatio={954 / designHeight} containerRef={containerRef}>
+        <Bubble x={1171} y={954}>
           Typescript
         </Bubble>
-        <Bubble xRatio={559 / designWidth} yRatio={999 / designHeight} containerRef={containerRef}>
+        <Bubble x={559} y={999}>
           NextJS
         </Bubble>
-        <Circle xRatio={1075 / designWidth} yRatio={993 / designHeight} containerRef={containerRef} />
-        <Bubble xRatio={230 / designWidth} yRatio={1010 / designHeight} containerRef={containerRef}>
-          Java
+        <Circle x={1075} y={993}></Circle>
+        <Bubble x={230} y={1010}>
+          AndroidStudio
         </Bubble>
-        <Bubble xRatio={851 / designWidth} yRatio={1013 / designHeight} containerRef={containerRef}>
+        <Bubble x={851} y={1013}>
           tailwindCSS
         </Bubble>
       </div>
--- src/sections/section7.js
+++ src/sections/section7.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Section7 = () => {
   const items = [
-    { i1: '2025.02.12', i2: '삼육보건대학교 의료정보과 졸업' },
-    { i1: '2024.02.01', i2: '육군병장 만기전역' },
-    { i1: '2020.09.29', i2: '자동차 운전면허 1종 취득' },
-    { i1: '2020.02.06', i2: '안양 부흥고등학교 졸업' },
+    { i1: "2025.02.12", i2: "삼육보건대학교 의료정보과 졸업" },
+    { i1: "2024.02.01", i2: "육군병장 만기전역" },
+    { i1: "2020.09.29", i2: "자동차 운전면허 1종 취득" },
+    { i1: "2020.02.06", i2: "안양 부흥고등학교 졸업" },
   ];
 
   const [selected, setSelected] = useState(items[0]);
@@ -15,30 +15,43 @@ const Section7 = () => {
   }
 
   return (
-    <section className='w-full h-dvh flex flex-col pt-[100px] pl-10 pb-[75px] relative'>
-      <h3 className='font-bold text-2xl leading-tight mt-[10vh] mx-0 mb-[21px] text-[#111]'>Carrer</h3>
-      <p className='m-0 max-w-60 font-medium text-sm leading-[18px] tracking-wide whitespace-pre-line'>
+    <section className="w-full h-dvh flex flex-col pt-[100px] pl-10 pb-[75px] relative">
+      <h3 className="font-bold text-2xl leading-tight mt-[10vh] mx-0 mb-[21px] text-[#111]">
+        Carrer
+      </h3>
+      <p className="m-0 max-w-60 font-medium text-sm leading-[18px] tracking-wide whitespace-pre-line">
         {`졸엽 경력과 자격증 입니다. 각 항목을 클릭하시면 상세 정보가 나옵니다.`}
       </p>
-
-      <div className='max-w-[324px] absolute top-[35vh] left-[324px] flex flex-wrap pl-10'>
-        {items.map((item, index) => (
-          <button onClick={() => clickHandler(item)} key={index}
-            className={`boxtext ${selected.i1 === item.i1 ? 'bg-black !text-white !border-none' : ''}`}
-          >
-            {item.i1}
-          </button>
-        ))}
-      </div>
-
-      {selected && (
-        <div className='absolute max-w-[275px] top-[40vh] left-[648px] w-full h-[230px]'>
-          <div className='absolute flex flex-col pl-[65px] gap-6 w-full'>
-            <h4 className='relative font-semibold text-xl leading-tight'>{selected.i1}</h4>
-            <p className='font-medium text-lg leading-tight tracking-wide text-[#111]'>{selected.i2}</p>
-          </div>
+      <div className="absolute top-14 left-0 w-full h-full">
+        <div className="max-w-[324px] absolute top-1/3 left-0 lg:left-[324px] flex flex-wrap pl-10">
+          {items.map((item, index) => (
+            <button
+              onClick={() => clickHandler(item)}
+              key={index}
+              className={`boxtext ${
+                selected.i1 === item.i1
+                  ? "bg-black !text-white !border-none"
+                  : ""
+              }`}
+            >
+              {item.i1}
+            </button>
+          ))}
         </div>
-      )}
+
+        {selected && (
+          <div className="absolute max-w-[275px] top-1/2 lg:top-1/3 left-0 lg:left-[648px] w-full h-[230px]">
+            <div className="absolute flex flex-col pt-3 pl-10 lg:pl-[65px] gap-6 w-full">
+              <h4 className="relative font-semibold text-xl leading-tight">
+                {selected.i1}
+              </h4>
+              <p className="font-medium text-lg leading-tight tracking-wide text-[#111]">
+                {selected.i2}
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
