276118657c3cd0866597ec9c02d136b4a88639c4
[kim2gon] <[heegonkim2001@gmail.com]>
2025-10-28 08:32:29 +0900

    readme 및 버블 위치 수정, 텍스트 조절

--- src/sections/section6.js
+++ src/sections/section6.js
@@ -147,19 +147,19 @@ const Section6 = () => {
         제가 가진 기술들을 애니메이션을 이용하여 재밌게 표현했습니다.
       </p>
       <div className="absolute left-0 bottom-0 right-0 h-[1000px] overflow-hidden">
-        <Bubble2 x={300} y={600} xLg={668} yLg={380}>
+        <Bubble2 x={220} y={620} xLg={668} yLg={380}>
           HTML
         </Bubble2>
-        <Bubble x={240} y={120} xLg={821} yLg={512}>
+        <Bubble x={400} y={580} xLg={821} yLg={512}>
           CSS
         </Bubble>
-        <Bubble x={200} y={750} xLg={552} yLg={553}>
+        <Bubble x={170} y={770} xLg={552} yLg={553}>
           JavaScript
         </Bubble>
-        <Bubble x={370} y={150} xLg={1031} yLg={619}>
+        <Bubble x={1031} y={619}>
           jQuery
         </Bubble>
-        <Bubble2 x={264} y={656}>
+        <Bubble2 x={1500} y={1500} xLg={264} yLg={656}>
           Photoshop
         </Bubble2>
         <Bubble x={871} y={720}>
@@ -168,36 +168,36 @@ const Section6 = () => {
         <Bubble x={642} y={732}>
           Figma
         </Bubble>
-        <Bubble2 x={260} y={600} xLg={1193} yLg={732}>
+        <Bubble2 x={300} y={820} xLg={1193} yLg={732}>
           React
         </Bubble2>
         <Circle x={710} y={803}></Circle>
         <Bubble x={1248} y={824}>
           Gsap
         </Bubble>
-        <Bubble x={1000} y={600} xLg={502} yLg={836}>
+        <Bubble x={1500} y={1500} xLg={502} yLg={836}>
           Fiber
         </Bubble>
         <Bubble x={1031} y={881}>
           Drei
         </Bubble>
-        <Bubble2 x={260} y={600} xLg={306} yLg={941}>
+        <Bubble x={360} y={940} xLg={306} yLg={941}>
           Framer Motion
-        </Bubble2>
+        </Bubble>
         <Bubble2 x={648} y={941}>
           ThreeJS
         </Bubble2>
-        <Bubble x={260} y={600} xLg={1171} yLg={954}>
+        <Bubble x={200} y={950} xLg={1171} yLg={954}>
           Typescript
         </Bubble>
-        <Bubble x={559} y={999}>
+        <Bubble x={1500} y={1500} xLg={559} yLg={999}>
           Next.js
         </Bubble>
         <Circle x={1075} y={993}></Circle>
-        <Bubble x={1000} y={300} xLg={230} yLg={1010}>
+        <Bubble x={1500} y={1500} xLg={230} yLg={1010}>
           Python
         </Bubble>
-        <Bubble x={260} y={600} xLg={851} yLg={1013}>
+        <Bubble x={340} y={690} xLg={851} yLg={1013}>
           tailwind CSS
         </Bubble>
       </div>
